8ec05831e9dcc5bd2b1c80ac59e723fbb8648103
Rodrigo Vieira (Trabalho) <rodrigo.conspena@gmail.com>
2013-07-03 17:59:30 -0300

    Varias implementacoes.

---
+++ Diretorio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DigoFramework
+{
+    public class Diretorio : Objeto
+    {
+        #region CONSTANTES
+
+        #endregion
+
+        #region ATRIBUTOS E PROPRIEDADES
+
+        private String _dirDiretorio = String.Empty;
+        public String dirDiretorio
+        {
+            get { return _dirDiretorio; }
+            set
+            {
+                _dirDiretorio = value;
+                System.IO.Directory.CreateDirectory(_dirDiretorio);
+            }
+        }
+
+        #endregion
+
+        #region CONSTRUTORES
+
+        #endregion
+
+        #region MÉTODOS
+
+        #endregion
+    }
+}
---
+++ Ftp.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace DigoFramework
+{
+    public class Ftp : Objeto
+    {
+        #region CONSTANTES
+
+        #endregion
+
+        #region ATRIBUTOS E PROPRIEDADES
+
+        private String _strPassword = String.Empty;
+        public String strPassword { get { return _strPassword; } set { _strPassword = value; } }
+
+        private String _strServer = String.Empty;
+        public String strServer { get { return _strServer; } set { _strServer = value; } }
+
+        private String _strUser = String.Empty;
+        public String strUser { get { return _strUser; } set { _strUser = value; } }
+
+        #endregion
+
+        #region CONSTRUTORES
+        
+        public Ftp(String strServer, String strUser, String strPassword)
+        {
+            #region VARIÁVEIS
+
+            this.strServer = strServer;
+            this.strUser = strUser;
+            this.strPassword = strPassword;
+
+            #endregion
+
+            #region AÇÕES
+            #endregion
+        }
+        
+        #endregion
+
+        #region MÉTODOS
+
+
+        public void uploadArquivo(string dirArquivo)
+        {
+            FileInfo objFileInfo = new FileInfo(dirArquivo);
+            string uri = "ftp://" + strServer + "/" + objFileInfo.Name;
+            FtpWebRequest objFtpWebRequest;
+
+            // Cria objeto FtpWebRequest
+            objFtpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + strServer + "/" + objFileInfo.Name));
+            // Cria credenciais
+            objFtpWebRequest.Credentials = new NetworkCredential(strUser, strPassword);
+            // Desativa a conexão ao terminar
+            objFtpWebRequest.KeepAlive = false;
+            // Especifica o comando que será executado
+            objFtpWebRequest.Method = WebRequestMethods.Ftp.UploadFile;
+            // Especifica que o arquivo é binário
+            objFtpWebRequest.UseBinary = true;
+            // Notifica o servidor do tamanho do arquivo que será enviado
+            objFtpWebRequest.ContentLength = objFileInfo.Length;
+            // Seta o tamanho do buffer
+            int intBuffTamanho = 2048;
+            byte[] buff = new byte[intBuffTamanho];
+            int intContentLen;
+            // Abre um Stream (System.IO.FileStream) para ler o arquivo que será enviado
+            FileStream objFileStream = objFileInfo.OpenRead();
+            try
+            {
+                // Stream to which the file to be upload is written
+                Stream objStream = objFtpWebRequest.GetRequestStream();
+                // Read from the file stream 2kb at a time
+                intContentLen = objFileStream.Read(buff, 0, intBuffTamanho);
+                // Till Stream content ends
+                while (intContentLen != 0)
+                {
+                    // Write Content from the file stream to the 
+                    // FTP Upload Stream
+                    objStream.Write(buff, 0, intContentLen);
+                    intContentLen = objFileStream.Read(buff, 0, intBuffTamanho);
+                }
+                // Close the file stream and the Request Stream
+                objStream.Close();
+                objFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Erro("Erro ao fazer Upload do arquivo.\n" + ex.Message, Erro.ErroTipo.Ftp);
+            }
+        }
+
+        #endregion
+    }
+}
