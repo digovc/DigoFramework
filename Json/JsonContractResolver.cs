3ab8279747fe2f8a9ee41d2a53423e8e9135f22d
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2016-01-12 16:58:58 -0200

    - Implementação do processo de salvamento. - Implementação de funções do menu principal.

---
+++ Json/JsonContractResolver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Newtonsoft.Json.Serialization;
+
+namespace DigoFramework.Json
+{
+    internal class JsonContractResolver : DefaultContractResolver
+    {
+        #region Constantes
+
+        #endregion Constantes
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Construtores
+
+        #endregion Construtores
+
+        #region Métodos
+
+        protected override string ResolvePropertyName(string strPropertyName)
+        {
+
+            #region Variáveis
+
+            #endregion Variáveis
+
+            #region Ações
+            try
+            {
+                if (string.IsNullOrEmpty(strPropertyName))
+                {
+                    return base.ResolvePropertyName(strPropertyName);
+                }
+
+                strPropertyName = "_" + strPropertyName;
+
+                return base.ResolvePropertyName(strPropertyName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+            }
+            #endregion Ações
+        }
+
+        #endregion Métodos
+
+        #region Eventos
+
+        #endregion Eventos
+    }
+}
