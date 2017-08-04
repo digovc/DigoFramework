3da8116dd754c28d746a5499424e3d364478ff98
Rodrigo Daros <rodrigo.conspena@gmail.com>
2017-08-04 15:02:15 -0300

    Ideal:
    - Compactar os dados para serem enviados para os aparelhos.

--- Utils.cs
+++ Utils.cs
@@ -1,4 +1,5 @@
 ﻿using DigoFramework.Import;
+using Ionic.Zlib;
 using System;
 using System.Diagnostics;
 using System.Globalization;
@@ -47,6 +48,46 @@ namespace DigoFramework
             return Process.Start("notepad", dirFile);
         }
 
+        public static string compactar(string str)
+        {
+            //var arrBte = Encoding.UTF8.GetBytes(str);
+
+            //using (var objMemoryStreamIn = new MemoryStream(arrBte))
+            //using (var objMemoryStreamOut = new MemoryStream())
+            //{
+            //    using (var objGZipStream = new GZipStream(objMemoryStreamOut, CompressionMode.Compress))
+            //    {
+            //        objMemoryStreamIn.CopyTo(objGZipStream);
+            //    }
+
+            //    return Convert.ToBase64String(objMemoryStreamOut.ToArray());
+            //}
+
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+
+            MemoryStream ms = new MemoryStream();
+
+            using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
+            {
+                zip.Write(buffer, 0, buffer.Length);
+            }
+
+            ms.Position = 0;
+
+            MemoryStream outStream = new MemoryStream();
+
+            byte[] compressed = new byte[ms.Length];
+
+            ms.Read(compressed, 0, compressed.Length);
+
+            byte[] gzBuffer = new byte[compressed.Length + 4];
+
+            Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
+            Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
+
+            return Convert.ToBase64String(gzBuffer);
+        }
+
         /// <summary>
         /// Verificar se o primeiro valor é igual a algum dos outros passados na sequência.
         /// </summary>
@@ -77,6 +118,22 @@ namespace DigoFramework
             }
 
             return false;
+        }
+
+        public static string descompactar(string str)
+        {
+            var arrBte = Encoding.UTF8.GetBytes(str);
+
+            using (var objMemoryStreamIn = new MemoryStream(arrBte))
+            using (var objMemoryStreamOut = new MemoryStream())
+            {
+                using (var objGZipStream = new GZipStream(objMemoryStreamIn, CompressionMode.Decompress))
+                {
+                    objGZipStream.CopyTo(objMemoryStreamOut);
+                }
+
+                return Encoding.UTF8.GetString(objMemoryStreamOut.ToArray());
+            }
         }
 
         /// <summary>
